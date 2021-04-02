e1fc215422af0bc1265a7526fd4926fdadfd29ef
AbdiMaulana1802 <abdimaulana200318@gmail.com>
2021-04-02 13:53:23 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_crud_datapegawai/pages/employee.dart';
+import 'package:flutter_crud_datapegawai/providers/employee_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => EmployeeProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Employee(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
